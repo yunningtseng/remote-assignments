538019a8e680eab37ccd4f8ef48f184af5244369
yunningtseng <71021669+yunningtseng@users.noreply.github.com>
2022-03-31 16:46:39 +0800

    first commit

---
+++ week-2/App.js
@@ -0,0 +1,27 @@
+// var context__box2 = document.querySelector(".context__box2");
+// var bottom = document.querySelector(".bottom");
+// var Menu = document.querySelector(".Menu");
+// var image = document.getElementById("image");
+
+const context__message = document.getElementById("context__message");
+context__message.addEventListener("click", () => {
+  context__message.textContent = "Have a Good Time!";
+});
+
+// bottom.addEventListener("click", () => {
+//   if (context__box2.style.display === "" || context__box2.style.display === "none") {
+//     context__box2.style.display = "block";
+//   } else {
+//     context__box2.style.display = "none";
+//   }
+// });
+
+// image.addEventListener("click", () => {
+//   if (Menu.style.display === "" || Menu.style.display === "none") {
+//     Menu.style.display = "block";
+//     image.src = "./img/x.png";
+//   } else {
+//     Menu.style.display = "none";
+//     image.src = "./img/menu.png";
+//   }
+// });
