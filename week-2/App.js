e19d27694e2eb8556114b297d01a903bc2ee0e66
yunningtseng <71021669+yunningtseng@users.noreply.github.com>
2022-04-01 14:08:03 +0800

    - feat: add toggle btn and navbar

--- week-2/App.js
+++ week-2/App.js
@@ -1,27 +1,38 @@
-// var context__box2 = document.querySelector(".context__box2");
-// var bottom = document.querySelector(".bottom");
-// var Menu = document.querySelector(".Menu");
-// var image = document.getElementById("image");
+// H_ toggle navbar
+let navImage = document.getElementById("nav__image");
+let navItemMobile = document.querySelector(".nav__item--mobile");
 
-const context__message = document.getElementById("context__message");
-context__message.addEventListener("click", () => {
-  context__message.textContent = "Have a Good Time!";
+navImage.addEventListener("click", () => {
+  if (
+    navItemMobile.style.display === "" ||
+    navItemMobile.style.display === "none"
+  ) {
+    navItemMobile.style.display = "block";
+    navImage.src = "img/x.png";
+  } else {
+    navItemMobile.style.display = "none";
+    navImage.src = "img/menu.png";
+  }
 });
 
-// bottom.addEventListener("click", () => {
-//   if (context__box2.style.display === "" || context__box2.style.display === "none") {
-//     context__box2.style.display = "block";
-//   } else {
-//     context__box2.style.display = "none";
-//   }
-// });
+// H_ click text
+let contextMessage = document.getElementById("context__message");
 
-// image.addEventListener("click", () => {
-//   if (Menu.style.display === "" || Menu.style.display === "none") {
-//     Menu.style.display = "block";
-//     image.src = "./img/x.png";
-//   } else {
-//     Menu.style.display = "none";
-//     image.src = "./img/menu.png";
-//   }
-// });
+contextMessage.addEventListener("click", () => {
+  contextMessage.textContent = "Have a Good Time!";
+});
+
+// H_ toggle box
+let contexBtn = document.querySelector(".contex__btn");
+let contextSection2 = document.querySelector(".context__section2");
+
+contexBtn.addEventListener("click", () => {
+  if (
+    contextSection2.style.display === "" ||
+    contextSection2.style.display === "none"
+  ) {
+    contextSection2.style.display = "block";
+  } else {
+    contextSection2.style.display = "none";
+  }
+});
