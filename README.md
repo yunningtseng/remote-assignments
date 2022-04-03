b4cf7f8178d47ae105ed881f13d247d1814fc2b6
yunningtseng <71021669+yunningtseng@users.noreply.github.com>
2022-04-03 23:31:27 +0800

    - week-2

---
+++ README.md
@@ -0,0 +1,5 @@
+# Remote Assignments
+
+[week-1](https://yunningtseng.github.io/remote-assignments/week-1/)
+
+[week-2](https://yunningtseng.github.io/remote-assignments/week-2/assignment-4/)
--- week-2/assignment-4/app.js
+++ week-2/assignment-4/app.js
@@ -1,12 +1,9 @@
-// H_ toggle navbar
+// > toggle navbar
 let navImage = document.getElementById("nav__image");
 let navItemMobile = document.querySelector(".nav__item--mobile");
 
 navImage.addEventListener("click", () => {
-  if (
-    navItemMobile.style.display === "" ||
-    navItemMobile.style.display === "none"
-  ) {
+  if (navItemMobile.style.display !== "block") {
     navItemMobile.style.display = "block";
     navImage.src = "img/x.png";
   } else {
@@ -15,22 +12,19 @@ navImage.addEventListener("click", () => {
   }
 });
 
-// H_ click text
+// > click text
 let contextMessage = document.getElementById("context__message");
 
 contextMessage.addEventListener("click", () => {
   contextMessage.textContent = "Have a Good Time!";
 });
 
-// H_ toggle box
+// > toggle box
 let contexBtn = document.querySelector(".contex__btn");
 let contextSection2 = document.querySelector(".context__section2");
 
 contexBtn.addEventListener("click", () => {
-  if (
-    contextSection2.style.display === "" ||
-    contextSection2.style.display === "none"
-  ) {
+  if (contextSection2.style.display !== "block") {
     contextSection2.style.display = "block";
   } else {
     contextSection2.style.display = "none";
