2520a9c86dd43a382fa79298bb7435e23865eb8f
yunningtseng <71021669+yunningtseng@users.noreply.github.com>
2022-04-11 22:58:19 +0800

    week 3 assignment

--- week-3/routes/getData.js
+++ week-3/routes/getData.js
@@ -5,18 +5,18 @@ const router = express();
 // * http://localhost:3000/getData
 router.get("/", (req, res) => {
   // * http://localhost:3000/getData?number=result
+  // * number 一定是文字或 undefined
   let { number } = req.query;
   // let { number, y, x } = req.query;
   // console.log('x:', x);
   // console.log('y:', y);
 
-  // > 一進入 getData 時 number 為空，!number 判斷為 true
-  if (!number) {
+  // - 一進入 getData 時 number 為空，判斷為 true
+  if (number == null || number == "") {
     res.send("Lack of Parameter");
-    // > 若 number 非數字或非正整數，判斷為 true
-  } else if (isNaN(number) || number <= 0) {
+    // - 若 number 非數字或非正整數，判斷為 true
+  } else if (!Number.isInteger(Number(number)) || number <= 0) {
     res.send("Wrong Parameter");
-    // > 計算加總 1 to i
   } else {
     let result = 0;
     for (i = 1; i <= number; i++) {
